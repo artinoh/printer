a2dfb5cdb41d276b0cf712eb9f3ac286dafc991d
Artin Ohanessian <artinohanessian@gmail.com>
2023-05-05 10:07:55 -0700

    adding input string

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include "char_matrix.h"
 
-int main()
+int main(int argc, char** argv)
 {
-
-    std::string str = "TESTING";
+    std::string str;
+    if (argc > 1)
+    {
+        str = argv[1];
+    }
+    else
+    {
+        str = "TESTING";
+    }
     char_matrix cm(str);
     cm.print();
     //cm.print_mapped_chars();
